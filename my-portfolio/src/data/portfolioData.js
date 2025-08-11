3cc9bf5a9af9f5253d11466cd32ce4ef7edb880b
CODE-Der <xxxcagang@gmail.com>
2025-08-12 00:13:50 +0800

    feat: created porfolioData.js file for information inputs

---
+++ my-portfolio/src/data/portfolioData.js
@@ -0,0 +1,33 @@
+export const portfolioData = {
+    name: "Xander Jay C. Cagang",
+    tagline: "",
+    about: "",
+    education: [
+        {
+
+        },
+        {
+
+        },
+        {
+
+        },
+    ],
+    projects: [
+        {
+
+        },
+        {
+
+        },
+        {
+            
+        },
+    ],
+    contact: {
+        email: "",
+        github: "",
+        facebook: "",
+        instagram: "",
+    }
+}
