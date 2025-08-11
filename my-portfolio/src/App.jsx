7bff9f6ff5866d6cc1b18fcf48955687150881ba
CODE-Der <xxxcagang@gmail.com>
2025-08-12 00:05:34 +0800

    feat: setup vite + react + tailwind

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-4xl font-italic text-blue-500">Hello tailwind! pls work</h1>
+  )
+}
+
+export default App
