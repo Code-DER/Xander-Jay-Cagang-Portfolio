496553ecf719af9803c2c89f68bf37d3340dc4a3
CODE-Der <xxxcagang@gmail.com>
2025-09-05 13:36:59 +0800

    refactor: delete components for new design

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,21 +1,10 @@
 import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Education from './components/Education'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
 
 function App() {
 
   return (
     <div className="scroll-smooth">
-      <Navbar />
-      <Hero />
-      <About />
-      <Education />
-      <Projects />
-      <Contact />
+      Hello
     </div>
   )
 }
