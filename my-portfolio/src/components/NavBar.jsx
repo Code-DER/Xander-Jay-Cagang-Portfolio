93f7f8d6bce6a14a4b0cce31a2ff57cc6628751a
CODE-Der <xxxcagang@gmail.com>
2025-09-05 23:59:46 +0800

    refactor: changed layout of navbar

--- my-portfolio/src/components/NavBar.jsx
+++ my-portfolio/src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import { FaHome, FaUser, FaFolder, FaRegAddressBook} from 'react-icons/fa';
 
 export default function NavBar() {
     const links = [
-        { name: "Home", path: "/" },
-        { name: "About", path: "/about" },
-        { name: "Projects", path: "/projects" },
-        { name: "Resume", path: "/resume" },
-    ]
+        { name: "Home", path: "/", icon: <FaHome /> },
+        { name: "About", path: "/about", icon: <FaUser /> },
+        { name: "Projects", path: "/projects", icon: <FaFolder /> },
+        { name: "Resume", path: "/resume", icon: <FaRegAddressBook /> },
+    ];
     
     return (
         <motion.nav
@@ -16,16 +17,22 @@ export default function NavBar() {
             transition={{ duration: 0.5 }}
             className="fixed top-0 left-0 w-full bg-white/80 backdrop-blur-md shadow-sm z-50"
         >
-            <div className="max-w-6xl mx-auto flex justify-center gap-8 py-3 text-gray-700 font-medium">
-                {links.map(link => (
-                    <Link
-                        key={link.id}
-                        to={link.path}
-                        className="hover:text-pink-500 transition colors"
-                    >
-                        {link.name}
-                    </Link>
-                ))}
+            <div className='max-w-6xl mx-auto flex items-center justify-between gap-8 py-3 text-gray-700 font-medium'>
+                <div className='text-xl font-bold'>
+                    Code-DER
+                </div>
+                <div className='flex gap-8'>
+                    {links.map(link => (
+                        <Link
+                            key={link.id}
+                            to={link.path}
+                            className="flex items-center gap-2 hover:text-pink-500 transition colors"
+                        >
+                            {link.icon}
+                            {link.name}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </motion.nav>
     )
