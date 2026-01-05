5afbfe2b1e5648c940b53b357a9861b2f3b0df98
CODE-Der <xxxcagang@gmail.com>
2026-01-05 23:23:54 +0800

    feat: edited Projects.jsx

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,7 +1,20 @@
 export default function Projects() {
     return (
         <>
-            <h1>Projects</h1>
+            <section>
+                <h1>Projects</h1>
+                <p>
+                    These are the projects that I've worked on to work on my coding skills.
+                    These projects are self-made projects based on interests. I've also included
+                    activities and projects that I've made in school.
+                </p>
+            </section>
+            <section>
+                <h1>My own Projects</h1>
+            </section>
+            <section>
+                <h1>School Projects</h1>
+            </section>
         </>
     );
 }
