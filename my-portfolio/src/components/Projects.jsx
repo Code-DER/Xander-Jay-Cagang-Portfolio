aeaf7657962ee7a5fdd073586af57f1d57ede045
CODE-Der <xxxcagang@gmail.com>
2026-01-06 23:04:28 +0800

    feat: created Card component for Projects

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import { portfolioData } from "../data/portfolioData";
+import Card from "./Card";
 
 export default function Projects() {
     return (
@@ -38,6 +40,40 @@ export default function Projects() {
                         Projects
                     </span>
                 </h1>
+                <div>
+                    {portfolioData.ownProjects.map((project, index) => {
+                        return (
+                            <Card data={portfolioData.ownProjects[index]}/> 
+                        )
+                    })}
+                </div>
+                {/* <div>
+                    {portfolioData.ownProjects.map((project, index) => {
+                        return (
+                            <div 
+                                key={index}
+                                className="w-[190px] h-[254px] group relative bg-[#171717] flex justify-center items-center cursor-pointer overflow-hidden shadow-[0px_0px_3px_1px_#00000088]"
+                            >
+                                <div
+                                    className="z-[1] h-[250px] w-[186px] flex items-center justify-center rounded-[5px] bg-[#171717] p-5 text-white"
+                                >
+                                    <h3>{project.title}</h3>
+                                    <p>{project.description}</p>
+                                    <div>
+                                        {project.tech.map((tech, index) => {
+                                            return (
+                                                <p>{tech}</p>
+                                            )
+                                        })}
+                                    </div>
+                                    <a href={project.link} target="_blank">
+                                        <div>link</div>
+                                    </a>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div> */}
             </motion.section>
             <section>
                 <h1 className="text-3xl font-bold text-center">
@@ -46,6 +82,38 @@ export default function Projects() {
                         Activities
                     </span>
                 </h1>
+                <div>
+                    {portfolioData.schoolActivities.map((activity, index) => {
+                        return (
+                            <Card data={portfolioData.schoolActivities[index]}/>
+                        )
+                    })}
+                </div>
+                {/* <div>
+                    {portfolioData.schoolActivities.map((activity, index) => {
+                        return (
+                            <div 
+                                key={index}
+                                
+                            >
+                                <div>
+                                <h3>{activity.title}</h3>
+                                <p>{activity.description}</p>
+                                <div>
+                                    {activity.tech.map((tech, index) => {
+                                        return (
+                                            <p>{tech}</p>
+                                        )
+                                    })}
+                                </div>
+                                <a href={activity.link} target="_blank">
+                                    <div>link</div>
+                                </a>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div> */}
             </section>
         </>
     );
